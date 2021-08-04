389684b1ea2e1c7d25fba54c819d940cea4d4a0d
Kouki Badr <badreddinekouki1998@gmail.com>
2021-08-04 19:14:36 +0100

    add toast notification icons

---
+++ lib/resources/icons.dart
@@ -0,0 +1,5 @@
+const String SUCCESS_ICON = "assets/icons/success.png";
+const String INFO_ICON = "assets/icons/info.png";
+const String ERROR_ICON = "assets/icons/error.png";
+
+const String PACKAGE_NAME = "elegant_notification";
