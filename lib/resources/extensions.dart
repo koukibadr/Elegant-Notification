38dbb2812b51e0154f178d70c938f56491be2c40
Badr Kouki <36043466+koukibadr@users.noreply.github.com>
2023-04-30 19:29:13 +0100

    Remove unused extensions

--- lib/resources/extensions.dart
+++ lib/resources/extensions.dart
@@ -1,53 +1,6 @@
 import 'package:elegant_notification/resources/arrays.dart';
 import 'package:flutter/cupertino.dart';
 
-extension TextExtension on Text {
-  Text cloneTitle() {
-    return Text(
-      data ?? '',
-      style: style ??
-          const TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 14,
-          ),
-      locale: locale,
-      maxLines: maxLines,
-      overflow: overflow,
-      semanticsLabel: semanticsLabel,
-      softWrap: softWrap,
-      key: key,
-      strutStyle: strutStyle,
-      textAlign: textAlign,
-      textDirection: textDirection,
-      textHeightBehavior: textHeightBehavior,
-      textScaleFactor: textScaleFactor,
-      textWidthBasis: textWidthBasis,
-    );
-  }
-
-  Text cloneDescription() {
-    return Text(
-      data ?? '',
-      style: style ??
-          const TextStyle(
-            fontSize: 12,
-          ),
-      locale: locale,
-      maxLines: maxLines,
-      overflow: overflow,
-      semanticsLabel: semanticsLabel,
-      softWrap: softWrap,
-      key: key,
-      strutStyle: strutStyle,
-      textAlign: textAlign,
-      textDirection: textDirection,
-      textHeightBehavior: textHeightBehavior,
-      textScaleFactor: textScaleFactor,
-      textWidthBasis: textWidthBasis,
-    );
-  }
-}
-
 extension NotificationPositionExtension on NotificationPosition {
   Alignment get alignment {
     switch (this) {
