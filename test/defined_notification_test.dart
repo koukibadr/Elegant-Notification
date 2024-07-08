dae0a8d3771c8b2c52e542bc0fe1968186f12f13
koukibadr <badreddinekouki1998@gmail.com>
2024-07-08 22:23:11 +0100

    Test: predefined elegant notification widget testing

--- test/defined_notification_test.dart
+++ test/defined_notification_test.dart
@@ -3,46 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('Simple elegant notification test', () {
-    testWidgets(
-      'basic elegant notification',
-      (tester) async {
-        var basicElgantNotificaiton = ElegantNotification(
-          description: const Text(
-            'Elegant notification description',
-          ),
-          icon: const Icon(
-            Icons.add,
-          ),
-        );
-        await tester.pumpWidget(
-          MaterialApp(
-            home: Scaffold(
-              body: Center(
-                child: basicElgantNotificaiton,
-              ),
-            ),
-          ),
-        );
-        expect(
-          find.byType(Text),
-          findsOneWidget,
-        );
-        expect(
-          find.byType(LinearProgressIndicator),
-          findsOneWidget,
-        );
-        expect(
-          find.byType(Icon),
-          findsAtLeastNWidgets(2),
-        );
-        expect(
-          basicElgantNotificaiton.background,
-          equals(Colors.white),
-        );
-      },
-    );
-
+  group('Testing defined elegant notifications', () {
     testWidgets(
       'Success elegant notification',
       (tester) async {
@@ -50,47 +11,6 @@ void main() {
           description: const Text(
             'Simple success toast',
           ),
-          autoDismiss: true,
-          toastDuration: const Duration(seconds: 1),
-        );
-        await tester.pumpWidget(
-          MaterialApp(
-            home: Scaffold(
-              body: Center(
-                child: basicElgantNotificaiton,
-              ),
-            ),
-          ),
-        );
-        expect(
-          find.byType(Text),
-          findsOneWidget,
-        );
-        expect(
-          find.byType(LinearProgressIndicator),
-          findsOneWidget,
-        );
-        expect(
-          find.byIcon(Icons.check_circle),
-          findsOneWidget,
-        );
-        expect(
-          basicElgantNotificaiton.background,
-          equals(Colors.white),
-        );
-      },
-    );
-
-
-    testWidgets(
-      'Success elegant notification',
-      (tester) async {
-        var basicElgantNotificaiton = ElegantNotification.success(
-          description: const Text(
-            'Simple success toast',
-          ),
-          autoDismiss: true,
-          toastDuration: const Duration(seconds: 1),
         );
         await tester.pumpWidget(
           MaterialApp(
@@ -121,14 +41,12 @@ void main() {
     );
 
     testWidgets(
-      'Success elegant notification',
+      'Info elegant notification',
       (tester) async {
-        var basicElgantNotificaiton = ElegantNotification.success(
+        var basicElgantNotificaiton = ElegantNotification.info(
           description: const Text(
-            'Simple success toast',
+            'Simple info toast',
           ),
-          autoDismiss: true,
-          toastDuration: const Duration(seconds: 1),
         );
         await tester.pumpWidget(
           MaterialApp(
@@ -148,7 +66,7 @@ void main() {
           findsOneWidget,
         );
         expect(
-          find.byIcon(Icons.check_circle),
+          find.byIcon(Icons.info),
           findsOneWidget,
         );
         expect(
@@ -158,12 +76,40 @@ void main() {
       },
     );
 
-    // testWidgets('Test toast dismissal', (tester) async {
-    //   await tester.pump(const Duration(seconds: 1));
-    //   expect(
-    //     find.byType(ElegantNotification),
-    //     findsNothing,
-    //   );
-    // });
+    testWidgets(
+      'Info elegant notification',
+      (tester) async {
+        var basicElgantNotificaiton = ElegantNotification.error(
+          description: const Text(
+            'Simple error toast',
+          ),
+        );
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: basicElgantNotificaiton,
+              ),
+            ),
+          ),
+        );
+        expect(
+          find.byType(Text),
+          findsOneWidget,
+        );
+        expect(
+          find.byType(LinearProgressIndicator),
+          findsOneWidget,
+        );
+        expect(
+          find.byIcon(Icons.add_circle_outlined),
+          findsOneWidget,
+        );
+        expect(
+          basicElgantNotificaiton.background,
+          equals(Colors.white),
+        );
+      },
+    );
   });
 }
